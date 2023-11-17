04c7b838243f6ba3533ad38e79855c7ca5d29e0b
AneeqMuneer <aneeq.muneer03@gmail.com>
2023-11-17 15:41:23 +0500

    did some work on brute and quick hull but not final

--- CHS_App/static/CHS_App/brute_force.js
+++ CHS_App/static/CHS_App/brute_force.js
@@ -1,1 +1,191 @@
 console.log('brute force');
+
+
+// Function to input points
+function InputPoints() {
+
+    const coordinates = JSON.parse(localStorage.getItem("coordinates")) || [];
+
+    for (let i = 0; i < coordinates.length; i++) {
+        if (!(!isNaN(coordinates[i].x) && !isNaN(coordinates[i].y))) {
+            alert("Invalid data found");
+            coordinates = [];
+        }
+    }
+
+    return coordinates;
+}
+
+async function drawcheckingGraph(selectedVerticeX, selectedVerticeY, unSelectedVerticeX, unSelectedVerticeY) {
+    GRAPH = document.getElementById('graphs-div');
+    var trace1 = {
+        x: unSelectedVerticeX,
+        y: unSelectedVerticeY,
+        mode: 'markers',
+        type: 'scatter',
+        name: "Unselected Vertices",
+        marker: { size: 12 }
+    }
+    var trace2 = {
+        x: selectedVerticeX,
+        y: selectedVerticeY,
+        mode: 'lines+markers',
+        type: 'scatter',
+        name: "Selected Vertices",
+        marker: { size: 18, color: 'green'}
+    }
+    data = [trace1, trace2, { title: 'Brute Force' }];
+    Plotly.newPlot('graphs-div', data);
+}
+
+async function drawGraph(selectedVerticeX, selectedVerticeY, unSelectedVerticeX, unSelectedVerticeY) {
+    GRAPH = document.getElementById('graphs-div');
+    var trace1 = {
+        x: unSelectedVerticeX,
+        y: unSelectedVerticeY,
+        mode: 'markers',
+        type: 'scatter',
+        name: "Unselected Vertices",
+        marker: { size: 12 }
+    }
+    var trace2 = {
+        x: selectedVerticeX,
+        y: selectedVerticeY,
+        mode: 'lines+markers',
+        type: 'scatter',
+        name: "Selected Vertices",
+        marker: { size: 12 }
+    }
+    data = [trace1, trace2, { title: 'Brute Force' }];
+    Plotly.newPlot('graphs-div', data);
+}
+
+// finds index
+function find(arr , val){
+    for(let i = 0 ; i < arr.length ; i++)
+    {
+        if(arr[i] === val)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+async function brute_force() {
+    const points = InputPoints();
+    const n = points.length;
+
+    if (n === 0) {
+        alert("No points")
+        return;
+    }
+    if (n < 3) {
+        alert("Enter at least three points.");
+        return;
+    };
+    let x = [];
+    let y = [];
+    let selectedx = [];
+    let selectedy = [];
+    let unselectedx = [];
+    let unselectedy = [];
+    let total = 0, val;
+
+    for (let i = 0; i < n; i++) {
+        x = x + points[i].x;
+        y = y + points[i].y;
+    }
+    let hull = new Array(n).fill(-1);
+
+    for (let i = 0; i < n; i++) {
+
+        for (let j = 0; j < n; j++) {
+            if (i !== j) {
+                let flag2 = true;
+
+                for (let k = 0; k < n; k++) {
+                    if (i !== k && j !== k) {
+                        const val = (x[j] - x[i]) * (y[k] - y[i]) - (y[j] - y[i]) * (x[k] - x[i]);
+                        if (val <= 0) {
+                            flag2 = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (flag2) {
+                    hull[i] = j;
+                    total++;
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        unselectedx.push(x[i]);
+        unselectedy.push(y[i]);
+    }
+
+    let count = 0, j, savedx, savedy, i = 0;
+    let done = false;
+    while (true) {
+        if (count == total) {
+            break;
+        }
+
+        if (hull[i] != -1) {
+
+            if (done === false) {
+                selectedx.push(x[i]);
+                selectedy.push(y[i]);
+                val = find(unselectedx , x[i]);
+                unselectedx.splice(val , 1);
+                unselectedy.splice(val , 1);
+                count++;
+                done = true;
+            }
+
+            for (j = 0; j <= hull[i]; j++) {
+                selectedx.push(x[j]);
+                selectedy.push(y[j]);
+                val = find(unselectedx , x[j]);
+                if(val != -1)
+                {
+                    unselectedx.splice(val , 1);
+                    unselectedy.splice(val , 1);
+                }
+
+                // call displayer
+                // console.log("Draw Graph");
+                // await new Promise((resolve) => setTimeout(resolve, 1000)); // Change the delay time as needed
+                // await drawGraph(selectedx, selectedy, unselectedx, unselectedy);
+
+                console.log(selectedx);
+                console.log(selectedy);
+                console.log(unselectedx);
+                console.log(unselectedy);
+
+                savedx = selectedx.pop();
+                savedy = selectedy.pop();
+                unselectedx.push(savedx);
+                unselectedy.push(savedy);
+            }
+
+            selectedx.push(savedx);
+            selectedy.push(savedy);
+            unselectedx.pop();
+            unselectedy.pop();
+
+            i = hull[i];
+            count++;
+            continue;
+        }
+
+        i++;
+    }
+
+    console.log("Draw Graph");
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Change the delay time as needed
+    await drawGraph(selectedx, selectedy, unselectedx, unselectedy);
+}
--- CHS_App/static/CHS_App/quick_hull.js
+++ CHS_App/static/CHS_App/quick_hull.js
@@ -1,1 +1,264 @@
-console.log("quick hull")
+console.log("quick hull");
+let selectedx = [];
+let selectedy = [];
+let unselectedx = [];
+let unselectedy = [];
+
+// Function to input points
+function InputPoints() {
+
+    const coordinates = JSON.parse(localStorage.getItem("coordinates")) || [];
+
+    for (let i = 0; i < coordinates.length; i++) {
+        if (!(!isNaN(coordinates[i].x) && !isNaN(coordinates[i].y))) {
+            alert("Invalid data found");
+            coordinates = [];
+        }
+    }
+
+    return coordinates;
+}
+
+// finds the index of a point in the array
+function find(p1x, p1y, x, y) {
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === p1x) {
+            if (y[i] === p1y) {
+                return i;
+            }
+        }
+    }
+
+    return -1;
+}
+
+// sorts points in ascending order
+function sort(x, y, n) {
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (x[j] > x[j + 1]) {
+                let temp1 = x[j];
+                x[j] = x[j + 1];
+                x[j + 1] = temp1;
+
+                let temp2 = y[j];
+                y[j] = y[j + 1];
+                y[j + 1] = temp2;
+            }
+            else if (x[j] === x[j + 1]) {
+                if (y[j] > y[j + 1]) {
+                    let temp1 = x[j];
+                    x[j] = x[j + 1];
+                    x[j + 1] = temp1;
+
+                    let temp2 = y[j];
+                    y[j] = y[j + 1];
+                    y[j + 1] = temp2;
+                }
+            }
+        }
+    }
+}
+
+// calculates slope between two points
+function slope(p1x, p1y, p2x, p2y) {
+    return (p2y - p1y) / (p2x - p1x);
+}
+
+// calculates the y intercept
+function intercept(p1x, p1y, p2x, p2y, m) {
+    return p1y - (m * p1x);
+}
+
+// finds the list of all points above the line
+function calcabove(p1x, p1y, p2x, p2y, x, y) {
+
+    let m = slope(p1x, p1y, p2x, p2y)
+    let b = intercept(p1x, p1y, p2x, p2y, m);
+
+    let pointsabove = [];
+
+    for (let i = 0; i < x.length; i++) {
+        let val = m * x[i] + b;
+        // console.log(`Point (${x[i]}, ${y[i]}) is compared to Line Y = ${val} and y[i] = ${y[i]}`);
+        if (y[i] > val) {
+            pointsabove.push(i);
+        }
+    }
+
+    return pointsabove;
+}
+
+// finds the list of all points below the line
+function calcbelow(p1x, p1y, p2x, p2y, x, y) {
+
+    let m = slope(p1x, p1y, p2x, p2y)
+    let b = intercept(p1x, p1y, p2x, p2y, m);
+
+    let pointsbelow = [];
+
+    for (let i = 0; i < x.length; i++) {
+        let val = m * x[i] + b;
+        // console.log(`Point (${x[i]}, ${y[i]}) is compared to Line Y = ${val} and y[i] = ${y[i]}`);        
+        if (y[i] < val) {
+            pointsbelow.push(i);
+        }
+    }
+
+    return pointsbelow;
+}
+
+function farthest_distance(p1x, p1y, p2x, p2y, xpoints, ypoints) {
+    let far_distance = 0;
+    let farthest_index = -1;
+    let distance = 0;
+    let A, B, C;
+
+    // creating the equation
+    A = -1 * slope(p1x, p1y, p2x, p2y);
+    B = 1;
+    C = -1 * intercept(p1x, p1y, p2x, p2y, -1 * A);
+
+    for (let i = 0; i < xpoints.length; i++) {
+        distance = Math.abs(A * xpoints[i] + B * ypoints[i] + C) / Math.sqrt(A ** 2 + B ** 2);
+
+        if (distance > far_distance) {
+            far_distance = distance;
+            farthest_index = i;
+        }
+    }
+
+    return farthest_index;
+}
+
+function quickhull2(p1x, p1y, p2x, p2y, xpoints, ypoints, state) {
+
+    if (xpoints.length === 0) {
+        return;
+    }
+
+    if (state === "above" || state === "mid") {
+        let aboveindex = calcabove(p1x, p1y, p2x, p2y, xpoints, ypoints);
+        if (aboveindex.length === 0) {
+            return;
+        }
+        let abovex = [];
+        let abovey = [];
+        for (let i = 0; i < aboveindex.length; i++) {
+            abovex.push(xpoints[aboveindex[i]]);
+            abovey.push(ypoints[aboveindex[i]]);
+        }
+        if (abovex.length === 0) {
+            return;
+        }
+        let farthest_index = farthest_distance(p1x, p1y, p2x, p2y, abovex, abovey);
+        let farthestx = abovex[farthest_index];
+        let farthesty = abovey[farthest_index];
+        let val = find(farthestx, farthesty, unselectedx, unselectedy);
+        if (val === -1) {
+            return;
+        }
+        unselectedx.splice(val, 1);
+        unselectedy.splice(val, 1);
+        selectedx.push(farthestx);
+        selectedy.push(farthesty);
+        // call the displayer
+        console.log(selectedx);
+        console.log(selectedy);
+        // here the displayer call will end
+        quickhull2(p1x, p1y, farthestx, farthesty, unselectedx, unselectedy, "above");
+        quickhull2(farthestx, farthesty, p2x, p2y, unselectedx, unselectedy, "above");
+    }
+
+    if (state === "below" || state === "mid") {
+        let belowindex = calcbelow(p1x, p1y, p2x, p2y, xpoints, ypoints);
+        if (belowindex.length === 0) {
+            return;
+        }
+        let belowx = [];
+        let belowy = [];
+        for (let i = 0; i < belowindex.length; i++) {
+            belowx.push(xpoints[belowindex[i]]);
+            belowy.push(ypoints[belowindex[i]]);
+        }
+        if (belowx.length === 0) {
+            return;
+        }
+        let farthest_index = farthest_distance(p1x, p1y, p2x, p2y, belowx, belowy);
+        let farthestx = belowx[farthest_index];
+        let farthesty = belowy[farthest_index];
+        let val = find(farthestx, farthesty, unselectedx, unselectedy);
+        if (val === -1) {
+            return;
+        }
+        unselectedx.splice(val, 1);
+        unselectedy.splice(val, 1);
+        selectedx.push(farthestx);
+        selectedy.push(farthesty);
+        // call the displayer
+        console.log(selectedx);
+        console.log(selectedy);
+        // here the displayer call will end
+        quickhull2(p1x, p1y, farthestx, farthesty, unselectedx, unselectedy, "below");
+        quickhull2(farthestx, farthesty, p2x, p2y, unselectedx, unselectedy, "below");
+    }
+
+    return;
+}
+
+function quick_hull() {
+
+    const points = InputPoints();
+    const n = points.length;
+
+    if (n === 0) {
+        alert("No points")
+        return;
+    }
+    if (n < 3) {
+        alert("Enter at least three points.");
+        return;
+    };
+
+    let x = [];
+    let y = [];
+
+    for (let i = 0; i < n; i++) {
+        x.push(points[i].x);
+        y.push(points[i].y);
+
+        unselectedx.push(points[i].x);
+        unselectedy.push(points[i].y);
+    }
+
+    sort(x, y, n);
+
+    console.log("x and y Array:");
+    console.log(x);
+    console.log(y);
+
+    let p1x = x[0];
+    let p1y = y[0];
+    let p2x = x[n - 1];
+    let p2y = y[n - 1];
+
+
+    selectedx.splice(0, 0, x[0], x[n - 1]);
+    selectedy.splice(0, 0, y[0], y[n - 1]);
+    // draw graph once
+    selectedx.splice(0, 2);
+    selectedy.splice(0, 2);
+
+
+    quickhull2(p1x, p1y, p2x, p2y, x, y, "mid");
+    unselectedx.splice(0, 1);
+    unselectedy.splice(0, 1);
+    unselectedx.splice(n - 1, 1);
+    unselectedy.splice(n - 1, 1);
+
+    selectedx.splice(0, 0, x[0], x[n - 1]);
+    selectedy.splice(0, 0, y[0], y[n - 1]);
+    console.log(selectedx);
+    console.log(selectedy);
+}
